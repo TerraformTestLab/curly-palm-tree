9be6965dc5136e5f4a102e4b3281f65fda4c4c7e
Sujay Samanta <sujay.samanta@hashicorp.com>
2025-12-18 11:12:10 +0000

    Refactor HelloWorld function to use a constant for the welcome message

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
+const (
+	welcomeMessage = "Hello, World!"
+)
+
 func main() {
 	fmt.Println(HelloWorld())
 }
 
 // HelloWorld returns the classic greeting message
 func HelloWorld() string {
-	return "Hello, World!"
+	return welcomeMessage
 }
